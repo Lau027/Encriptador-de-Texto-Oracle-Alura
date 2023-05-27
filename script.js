1035ef36e190d2ac266d713326bdb9285b637c59
Laura Rodriguez <laurarodriguezsimanca@gmail.com>
2023-05-27 00:39:36 -0400

    agregar boton encriptar y desencriptar/ bloqueo de caracteres especiales y acentos

--- script.js
+++ script.js
@@ -1,76 +1,66 @@
-// REQUISITOS
+let ingresoTexto = document.getElementById("ingreso-texto");
+let salidaMensaje = document.getElementById("salida-mensaje");
 
-// no debe aceptar letras con acentos ni caracteres especiales
 
-const ingresoTexto = document.getElementById("ingreso-texto")
+function encriptar(mensaje){
 
-function encriptar(){
-
-    let llaves = [
-        ["e", "enter"],
-        ["i", "imes"],
-        ["a", "ai"],
-        ["o", "ober"],
-        ["u", "ufat"]
-    ]
-    const texto = ingresoTexto.value.toLowerCase();
-    var nuevoTexto = texto;
-    for (i = 0; i < llaves.length; i++){
-        nuevoTexto = nuevoTexto.replaceAll(llaves[i][0],llaves[i][1]);
-    }
-    
-    document.getElementById("salida-mensaje").innerHTML = nuevoTexto;
-    document.getElementById("salida-mensaje").style.height = "80%";
-    document.getElementById("salida-mensaje").style.textAlign = "start";
-    document.getElementById("dibujo").style.display = "none";
-    document.getElementById("boton-copiar").style.display = "block";
-    document.getElementById("mensaje-informacion").style.display = "none";
-}
-
-function desencriptar(){
-
-    let llaves = [
-        ["enter", "e"],
-        ["imes", "i"],
-        ["ai", "a"],
-        ["ober", "o"],
-        ["ufat", "u"]
-    ]
-    const texto = ingresoTexto.value.toLowerCase();
-    var nuevoTexto = texto;
+    let llaves = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    const mensajeEncriptado = ingresoTexto.value.toLowerCase();
+    var nuevoTexto = mensajeEncriptado;
 
     for (i = 0; i < llaves.length; i++){
         nuevoTexto = nuevoTexto.replaceAll(llaves[i][0],llaves[i][1]);
-    }
+    };
 
-    document.getElementById("salida-mensaje").innerHTML = nuevoTexto;
-    document.getElementById("salida-mensaje").style.height = "80%";
-    document.getElementById("salida-mensaje").style.textAlign = "start";
+    return nuevoTexto;
+}
+
+function botonEncriptar(){
+
+    if (ingresoTexto.value != 0){
+
+    let textoEncriptado = encriptar(ingresoTexto.value);
+    salidaMensaje.value = textoEncriptado;
+    salidaMensaje.style.height = "80%";
+    salidaMensaje.style.textAlign = "start";
     document.getElementById("dibujo").style.display = "none";
     document.getElementById("boton-copiar").style.display = "block";
     document.getElementById("mensaje-informacion").style.display = "none";
+
+    };
+};
+
+function desencriptar(mensaje){
+
+    let llaves = [["enter", "e"],["imes", "i"],["ai", "a"],["ober", "o"],["ufat", "u", ]];
+    const mensajeDesencriptado = ingresoTexto.value.toLowerCase();
+    var nuevoTexto = mensajeDesencriptado;
+
+    for (i = 0; i < llaves.length; i++){
+        nuevoTexto = nuevoTexto.replaceAll(llaves[i][0],llaves[i][1]);
+    };
+
+    return nuevoTexto;
 }
+
+function botonDesencriptar(){
+
+    if (ingresoTexto.value != 0){
+
+    let textoDesencriptado = desencriptar(ingresoTexto.value);
+    salidaMensaje.value = textoDesencriptado;
+    salidaMensaje.style.height = "80%";
+    salidaMensaje.style.textAlign = "start";
+    document.getElementById("dibujo").style.display = "none";
+    document.getElementById("boton-copiar").style.display = "block";
+    document.getElementById("mensaje-informacion").style.display = "none";
+
+    };
+};
 
 function copiar(){
 
-    var contenido = document.getElementById("salida-mensaje");
-    navigator.clipboard.writeText(contenido.innerHTML)
-}
-
-ingresoTexto.addEventListener("click", ()=>{
+    let contenido = document.getElementById("salida-mensaje");
+    navigator.clipboard.writeText(contenido.value)
     ingresoTexto.value = " ";
-    document.getElementById("dibujo").style.display = "block";
-    document.getElementById("boton-copiar").style.display = "none";
-    document.getElementById("mensaje-informacion").style.display = "block";
-    document.getElementById("salida-mensaje").style.textAlign = "center";
-    document.getElementById("salida-mensaje").textContent = "Ningún mensaje fue encontrado";
-    document.getElementById("salida-mensaje").style.height = "auto";
-})
-
-botonEncriptar = document.getElementById("boton-encriptar")
-
-if (texto != ""){
-    botonEncriptar.addEventListener("click", encriptar)
-} else {
-    console.log("error")
 }
