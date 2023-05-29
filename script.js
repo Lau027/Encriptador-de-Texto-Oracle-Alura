3f9862bf9be1f89bf1eed46adeddd9ffdbe0e293
Laura Rodriguez <laurarodriguezsimanca@gmail.com>
2023-05-29 15:18:00 -0400

    eliminacion de variable en botonEncriptar y botonDesencriptar

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ var ingresoTexto = document.getElementById("ingreso-texto");
 var salidaMensaje = document.getElementById("salida-mensaje");
 var llaves = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
 
-function encriptar(mensaje){
+function encriptar(){
 
     var nuevoTexto = ingresoTexto.value;
 
     for (i = 0; i < llaves.length; i++){
         nuevoTexto = nuevoTexto.replaceAll(llaves[i][0],llaves[i][1]);
-    }
+    };
     return nuevoTexto;
-}
+};
 
 function botonEncriptar(){
 
     if (ingresoTexto.value != 0){
 
-    var textoEncriptado = encriptar(ingresoTexto.value);
-    salidaMensaje.value = textoEncriptado;
+    salidaMensaje.value = encriptar();
     salidaMensaje.style.height = "80%";
     salidaMensaje.style.textAlign = "start";
     document.getElementById("dibujo").style.display = "none";
@@ -27,29 +26,26 @@ function botonEncriptar(){
     };
 };
 
-function desencriptar(mensaje){
+function desencriptar(){
 
     var nuevoTexto = ingresoTexto.value;
 
     for (i = 0; i < llaves.length; i++){
         nuevoTexto = nuevoTexto.replaceAll(llaves[i][1],llaves[i][0]);
     };
-
     return nuevoTexto;
-}
+};
 
 function botonDesencriptar(){
 
     if (ingresoTexto.value != 0){
 
-    var textoDesencriptado = desencriptar(ingresoTexto.value);
-    salidaMensaje.value = textoDesencriptado;
+    salidaMensaje.value = desencriptar();
     salidaMensaje.style.height = "80%";
     salidaMensaje.style.textAlign = "start";
     document.getElementById("dibujo").style.display = "none";
     document.getElementById("boton-copiar").style.display = "block";
     document.getElementById("mensaje-informacion").style.display = "none";
-
     };
 };
 
@@ -57,9 +53,9 @@ function copiar(){
 
     var contenido =  salidaMensaje;
     navigator.clipboard.writeText(contenido.value);
-    ingresoTexto.value = "Ingrese el texto aquí"
-}
+    ingresoTexto.value = "Ingrese el texto aquí";
+};
 
 ingresoTexto.addEventListener("click", () =>{
     ingresoTexto.value = " ";
-})
+});
